c8e93327a73c5757e0ab3a79065f9a8bda0d5840
Benyouceftebbi <benyoucef.tebbi@std.antalya.edu.tr>
2025-05-14 23:28:51 +0100

    new

--- app/admin/commandes/confirmes/loading.tsx
+++ app/admin/commandes/confirmes/loading.tsx
@@ -2,6 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
+
+    
     <div className="space-y-4">
       <div className="flex items-center justify-between">
         <Skeleton className="h-8 w-48" />
