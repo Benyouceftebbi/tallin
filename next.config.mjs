2aca1439ba909b2f14b430768dfa715e966bdc9e
Benyouceftebbi <benyoucef.tebbi@std.antalya.edu.tr>
2025-06-30 11:59:30 +0100

    new

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb', // or '20mb' if needed
+    },
+  },
 }
 
 export default nextConfig
